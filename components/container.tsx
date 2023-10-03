1c7ba616c5cd353db4b02fb75184b2e27b0e9db8
Gaurav Pandey <hi@pandeygaurav.com>
2023-10-03 20:12:14 +0530

    surprise

--- components/container.tsx
+++
@@ -1,7 +0,0 @@
-type ContainerProps = {
-  children: React.ReactNode
-}
-
-export default function Container({ children }: ContainerProps) {
-  return <div className="container space-y-5 max-w-2xl m-auto">{children}</div>
-}
